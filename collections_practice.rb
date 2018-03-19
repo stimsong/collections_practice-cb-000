0112f45bb7a862df80114b34743824889c03fcb0
Greg Stimson <stimsong@yahoo.com>
2018-03-19 20:48:43 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort { |left, right| left.length <=> right.length }
+end
+
+def swap_elements(array)
+  array [0], array[1], array[2] = array [0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each { |item| item[2] = "$" }
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?("a") }
+end
+
+def sum_array(array)
+  sum = 0
+  array.each {|e| sum += e}
+  sum
+end
+
+def add_s(array)
+#  [1,2].each_with_index.collect{|element, index| }
+  array.collect { |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end }
+end
